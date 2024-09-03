72c4a8c4f7fed4c0221f560c00a3c4a2a95b876b
Minifull6 <lxinyu855@gmail.com>
2024-09-03 19:19:49 +0800

    swirl_Start

---
+++ swirl_Start.R
@@ -0,0 +1,11 @@
+install.packages("swirl")
+packageVersion("swirl")
+
+# very time you want to use swirl, you need to first load the package.
+library(swirl)
+
+# you want the one called R Programming. Type the following from the R prompt to install this course
+install_from_swirl(“R Programming")
+
+# Start
+swirl()
