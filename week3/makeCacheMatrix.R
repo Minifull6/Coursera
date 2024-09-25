a09b8177abbf394e428c5971411d06982f1458b7
Minifull6 <lxinyu855@gmail.com>
2024-09-25 15:29:43 +0800

    Update makeCacheMatrix.R

--- week3/makeCacheMatrix.R
+++ week3/makeCacheMatrix.R
@@ -1,11 +1,17 @@
 makeCacheMatrix <- function(x = matrix()){
+    # give a variable to store the inverse
     m <- NULL
+    # set the value of the matrix
     set <- function(y){
         x <<- y
         m <<- NULL
     }
+    # get the value of the matrix
     get <- function() x
+    # set the inverse of the matrix
     set_inverse <- function(inverse) m <<- inverse
+    # get the cache inverse
     get_inverse <- function() m
+    # Return a list of functions to interact with the matrix and its inverse
     list(set = set, get = get, set_inverse = set_inverse, get_inverse = get_inverse)
 }
