36827981cb521d2eefac5a7834263f13e7551ac6
Minifull6 <lxinyu855@gmail.com>
2024-09-25 15:32:34 +0800

    Update CacheSolve.R

--- week3/CacheSolve.R
+++ week3/CacheSolve.R
@@ -1,11 +1,14 @@
 CacheSolve <- function(x, ...) {
+    #Check if the inverse is already cached
     m <- x$get_inverse()
+    # If cached inverse exists, return it
     if(!is.null(m)) {
         message("getting cached data")
         return(m)
     }
-    data <- x$get()
-    m <- solve(data, ...)
-    x$set_inverse(m)
-    m
+    # If not, calculate the inverse
+    data <- x$get() # Retrieve the matrix
+    m <- solve(data, ...) # Compute the inverse using the solve function
+    x$set_inverse(m) # Cache the calculated inverse
+    m # return the value of incerse
 }
